a113755a7fb49fad168dc14d7cb3b811d90c9a7e
MinKu Kang <tominku@gmail.com>
2024-08-01 16:31:10 +0900

    ..

---
+++ RLM_symbolic.py
@@ -0,0 +1,6 @@
+from sympy import *
+v_g, i1, i1p, R1, L1, dt = symbols('vg i1, i1p, R1, L1 dt')
+M = symbols('M')
+i2, i2p, R2, L2 = symbols('i2, i2p, R2, L2')
+
+-v_g + i
--- test_collect.py
+++ test_collect.py
@@ -1,6 +1,6 @@
 from sympy import *
-x, y, a, b, c, d = symbols('x y a b c d')
-expr = a*x + b*x + c*y + d*y
+x, y, a, b, c, d, const1, const2 = symbols('x y a b c d const1 const2')
+expr = a*x + b*x + c*y + d*y + const1 + const2
 print(expr)
 collected1 = expr.collect(x)
 collected2 = collected1.collect(y)
