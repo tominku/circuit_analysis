8c000db106860e402a08b8e90df2c3844fe59aa1
MinKu Kang <tominku@gmail.com>
2024-08-01 18:37:55 +0900

    ..

--- RLM_symbolic.py
+++ RLM_symbolic.py
@@ -3,4 +3,11 @@ v_g, i1, i1p, R1, L1, dt = symbols('vg i1, i1p, R1, L1 dt')
 M = symbols('M')
 i2, i2p, R2, L2 = symbols('i2, i2p, R2, L2')
 
--v_g + i
+expr1 = -v_g + i1*R1 + L1*(i1p-i1)/dt - M*(i2p-i2)/dt
+expr2 = i2*R2 + L2*(i2p-i2)/dt - M*(i1p-i1)/dt
+
+print(expr1)
+print(expr2)
+
+expr1 = expr1.collect(dt)
+print(expr1)
