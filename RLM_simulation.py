c11f30506ed32051fc096e4deced53121f3e098a
MinKu Kang <tominku@gmail.com>
2024-08-02 05:24:04 +0900

    ..

---
+++ RLM_simulation.py
@@ -0,0 +1,61 @@
+import numpy as np
+import numpy.linalg as LA
+
+# [L1/dt, -M/dt]
+# [-M/dt, L2/dt]
+# [-(-L1*i1 + M*i2 + R1*dt*i1 - dt*v_g)/dt, -(-L2*i2 + M*i1 + R2*dt*i2)/dt]
+
+dt = 0.01
+L1 = 12
+L2 = 12
+M = 4
+R1 = 20
+R2 = 20
+v_g_mag = 5
+v_g = v_g_mag
+is_input_wave = False
+#is_input_wave = True
+
+A = np.array([[L1/dt, -M/dt], [-M/dt, L2/dt]])
+print(A)
+
+#b = np.array([-(-L1*i1 + M*i2 + R1*dt*i1 - dt*v_g)/dt, -(-L2*i2 + M*i1 + R2*dt*i2)/dt])
+
+i = 0
+
+i1_list = []
+i2_list = []
+v_g_list = []
+t_list = []
+i1 = 0
+i2 = 0
+for step in range(800): 
+    t = dt*step   
+    v_g = v_g_mag
+    if is_input_wave:
+        freq = 1
+        v_g = v_g_mag*np.sin(2*np.pi*freq*t)
+    
+    b = np.array([-(-L1*i1 + M*i2 + R1*dt*i1 - dt*v_g)/dt, -(-L2*i2 + M*i1 + R2*dt*i2)/dt])        
+    i = LA.solve(A, b)
+    
+    i1 = i[0]
+    i2 = i[1]    
+    i1_list.append(i1)
+    i2_list.append(i2)
+    v_g_list.append(v_g)
+    t_list.append(t)
+    #print(f'a: {(-R*dt/L) + 1}, b: {v_g*dt/L}')
+
+import matplotlib.pyplot as plt
+i1_arr = np.array(i1_list)
+i2_arr = np.array(i2_list)
+v_g_arr = np.array(v_g_list)
+t_arr = np.array(t_list)
+plt.plot(t_arr, i1_arr, label='I1 (t)')
+plt.plot(t_arr, i2_arr, label='I2 (t)')
+v_g_plot_scale = 0.05
+#plt.plot(t_arr, v_g_plot_scale*v_g_arr, label=f'{v_g_plot_scale} * V_g(t)')
+plt.grid(True)
+plt.legend()
+plt.show()
--- RLM_symbolic.py
+++ RLM_symbolic.py
@@ -1,5 +1,5 @@
 from sympy import *
-v_g, i1, i1p, R1, L1, dt = symbols('vg i1, i1p, R1, L1 dt')
+v_g, i1, i1p, R1, L1, dt = symbols('v_g i1, i1p, R1, L1 dt')
 M = symbols('M')
 i2, i2p, R2, L2 = symbols('i2, i2p, R2, L2')
 
@@ -9,5 +9,26 @@ expr2 = i2*R2 + L2*(i2p-i2)/dt - M*(i1p-i1)/dt
 print(expr1)
 print(expr2)
 
-expr1 = expr1.collect(dt)
-print(expr1)
+expr1 = Poly(expr1, i1p, i2p)
+expr2 = Poly(expr2, i1p, i2p)
+
+expr1_coeffs = expr1.coeffs()
+expr2_coeffs = expr2.coeffs()
+
+A11 = expr1_coeffs[0]
+A12 = expr1_coeffs[1]
+b1 = -expr1_coeffs[2]
+
+A21 = expr2_coeffs[0]
+A22 = expr2_coeffs[1]
+b2 = -expr2_coeffs[2]
+
+A = [[A11, A12], [A21, A22]]
+b = [b1, b2]
+
+print(A[0])
+print(A[1])
+print(b)
+
+
+
